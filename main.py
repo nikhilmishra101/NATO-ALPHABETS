009e6cde28e7774ee0b734ef9d985c836559fd41
IEIrodov <nikhilindia30111999@gmail.com>
2021-03-11 21:12:23 +0530

    Adding Exception Handling

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import pandas as pd
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
 
 data = pd.read_csv("nato_phonetic_alphabet.csv")
-#print(data)
+
 
 new_dict = {row.letter:row.code for (index,row) in data.iterrows()}
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-user_input = input("Enter a word: ").upper()
-
-user_letters = [letter for letter in user_input]
-
-user_words = [new_dict[letter] for letter in user_letters if letter in new_dict]
-
-print(user_words)
+incorrect_input = True
+while incorrect_input:
+    user_input = input("Enter a word: ").upper()
+    try:
+        user_words = [new_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry only letters in the alphabet please.")
+    else:
+        print(user_words)
+        incorrect_input = False
 
 
 
